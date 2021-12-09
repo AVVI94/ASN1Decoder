c6b8cf901674ad39c0965d151317b7aa1874337b
AVVI94 <57726562+AVVI94@users.noreply.github.com>
2021-12-09 16:28:54 +0100

    Benchmarks

---
+++ Asn1DecoderNet5.Benchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Asn1DecoderNet5.Interfaces;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Asn1DecoderNet5.Benchmark
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Bench>();
+        }
+    }
+}
