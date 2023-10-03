bea8f2aa267f5a938dfe8dedc102714fdc64dce4
Jan Král <avvi@outlook.com>
2023-10-03 20:44:54 +0200

    More extension methods, cert/request checks reworked based on specifications, changes of source data in tests

--- Asn1DecoderNet5.UnitTests/DecoderTests.cs
+++ Asn1DecoderNet5.UnitTests/DecoderTests.cs
@@ -10,6 +10,56 @@ namespace Asn1DecoderNet5.UnitTests
     public class DecoderTests
     {
         [Fact]
+        public void GetCommonName_Issuer()
+        {
+            var tag = Decoder.Decode(Sources.NormalCertificate);
+            var ok = tag.TryGetCertificateSubjectItem(SubjectItemKind.CommonName, true, out var res);
+            Assert.True(ok);
+            Assert.Equal("I.CA Test Qualified 2 CA/ECC 06/2019", res[0]);
+        }
+        
+        [Fact]
+        public void Should_IsCertificate_ReturnsTrue()
+        {
+            var tag = Decoder.Decode(Sources.NormalCertificate);
+            var ok = tag.IsCertificate();
+            Assert.True(ok);
+        }
+        [Fact]
+        public void GetCommonName_Subject()
+        {
+            var tag = Decoder.Decode(Sources.NormalCertificate);
+            var ok = tag.TryGetCertificateSubjectItem(SubjectItemKind.CommonName, false, out var res);
+            Assert.True(ok);
+            Assert.Equal("EccTest CertRenewer", res[0]);
+        }
+        [Fact]
+        public void GetCommonName()
+        {
+            var tag = Decoder.Decode(Sources.NormalCertificate);
+            var ok = tag.TryGetSubjectItem(Encoding.OidEncoding.GetBytes(OID.COMMON_NAME), out var items);
+            Assert.True(ok);
+            Assert.NotEmpty(items);
+        }
+        [Fact]
+        public void GetKeyUsage()
+        {
+            var tag = Decoder.Decode(Sources.NormalCertificate);
+            var ku = tag.TryGetKeyUsage(out var keyUsage);
+            Assert.True(ku);
+            Assert.False(keyUsage.KeyUsages == null);
+        }
+        
+        [Fact]
+        public void ShouldNotGetKeyUsage()
+        {
+            var tag = Decoder.Decode(Sources.UndefinedLengthPfx);
+            var ku = tag.TryGetKeyUsage(out var keyUsage);
+            Assert.False(ku);
+            Assert.True(keyUsage.KeyUsages == null);
+        }
+
+        [Fact]
         public void DecodeNormalCertificate()
         {
             var tag = Decoder.Decode(Sources.NormalCertificate);
@@ -17,6 +67,7 @@ namespace Asn1DecoderNet5.UnitTests
 
             Assert.Equal(Results.NormalCertificate, decodedString);
         }
+
         [Fact]
         public void DecodeNormalCertificate_NoSeparator()
         {
@@ -24,30 +75,6 @@ namespace Asn1DecoderNet5.UnitTests
             var decodedString = Decoder.TagToString(tag, "", 128);
 
             Assert.Equal(Results.NormalCertificate_NoSeparator, decodedString);
-        }
-        [Fact]
-        public void DecodeNormalCertificate_NoLineLength()
-        {
-            var tag = Decoder.Decode(Sources.NormalCertificate);
-            var decodedString = Decoder.TagToString(tag, " | ", 0);
-
-            Assert.Equal(Results.NormalCertificate_NoLineLength, decodedString);
-        }
-        [Fact]
-        public void DecodeNormalCertificate_NoLineLength_NoSeparator()
-        {
-            var tag = Decoder.Decode(Sources.NormalCertificate);
-            var decodedString = Decoder.TagToString(tag, "", 0);
-
-            Assert.Equal(Results.NormalCertificate_NoLineLength_NoSeparator, decodedString);
-        }
-        [Fact]
-        public void DecodeNormalCertificate_NegativeLength()
-        {
-            var tag = Decoder.Decode(Sources.NormalCertificate);
-            var decodedString = Decoder.TagToString(tag, " | ", -1);
-
-            Assert.Equal(Results.NormalCertificate_NoLineLength, decodedString);
         }
 
         [Fact]
--- Asn1DecoderNet5/Extensions.cs
+++ Asn1DecoderNet5/Extensions.cs
@@ -8,6 +8,11 @@ namespace Asn1DecoderNet5
 {
     public static class Extensions
     {
+        static readonly byte[] _version0Sequence = new byte[] { 0x00 };
+        static readonly byte[] _version2Sequence = new byte[] { 0x02 };
+        static readonly byte[] _booleanTrueSequence = new byte[] { 0xFF };
+        static readonly byte[] _keyUsageOidSequence = new byte[] { 0x55, 0x1D, 0x0f };
+
         public static string ToOidString(this SubjectItemKind subjectItem)
         {
             return subjectItem switch
@@ -25,10 +30,8 @@ namespace Asn1DecoderNet5
                 _ => null//throw new ArgumentException($"Parameter '{nameof(subjectItem)}' has invalid value!", nameof(subjectItem)),
             };
         }
-        #region KeyUsage
-        static readonly byte[] _booleanTrueSequence = new byte[] { 0xFF };
-        static readonly byte[] _keyUsageOidSequence = new byte[] { 0x55, 0x1D, 0x0f };
 
+        #region KeyUsage
         /// <summary>
         /// Attempts to find and parse key usage from the structure
         /// </summary>
@@ -39,6 +42,18 @@ namespace Asn1DecoderNet5
         {
             keyUsage = default;
             try
+            {
+                //if the tag is cert, the KeyUsage location is somewhere in cert's extensions
+                if (IsCertificate(tag))
+                    tag = tag.Childs[0].Childs[tag.Childs[0].Childs.Count - 1].Childs[0];
+                return FindKeyUsage(tag, ref keyUsage);
+            }
+            catch
+            {
+                return false;
+            }
+
+            static bool FindKeyUsage(ITag tag, ref KeyUsage keyUsage)
             {
                 if (tag is KeyUsageTag { KeyUsage.KeyUsages: not null } kut)
                 {
@@ -53,13 +68,9 @@ namespace Asn1DecoderNet5
 
                 foreach (var t in tag.Childs)
                 {
-                    if (t.TryGetKeyUsage(out keyUsage))
+                    if (FindKeyUsage(t, ref keyUsage))
                         return true;
                 }
-                return false;
-            }
-            catch
-            {
                 return false;
             }
         }
@@ -71,6 +82,9 @@ namespace Asn1DecoderNet5
         internal static ITag GetKeyUsageBitStringTag(this ITag tag)
             => tag.Childs[2].Childs[0];
         #endregion
+
+        //X.509 v3 https://www.rfc-editor.org/rfc/rfc5280
+        //PKCS10 v1.7 https://datatracker.ietf.org/doc/html/rfc2986
 
         /// <summary>
         /// Attempts to find the value for requested OID. Certificate can contain more than one value for requested OID, the values are ordered in the 'items' list as they were found in the structure.
@@ -108,9 +122,9 @@ namespace Asn1DecoderNet5
         /// <param name="forIssuer">Indicates, whether to find the value for requested OID in the Issuer element or in the Subject element</param>
         /// <param name="items">The value</param>
         /// <returns><see langword="true" /> if any value was found, otherwise <see langword="false" /></returns>
-        public static bool TryGetSubjectItem(this ITag topLevelTag, SubjectItemKind subjectItem, bool forIssuer, out List<string> items)
+        public static bool TryGetCertificateSubjectItem(this ITag topLevelTag, SubjectItemKind subjectItem, bool forIssuer, out List<string> items)
         {
-            return TryGetSubjectItem(topLevelTag, subjectItem.ToOidString(), forIssuer, out items);
+            return TryGetCertificateSubjectItem(topLevelTag, subjectItem.ToOidString(), forIssuer, out items);
         }
 
         /// <summary>
@@ -149,51 +163,11 @@ namespace Asn1DecoderNet5
         /// <param name="forIssuer">Indicates, whether to find the value for requested OID in the Issuer element or in the Subject element</param>
         /// <param name="items">The value</param>
         /// <returns><see langword="true" /> if any value was found, otherwise <see langword="false" /></returns>
-        public static bool TryGetSubjectItem(this ITag topLevelTag, string oid, bool forIssuer, out List<string> items)
+        public static bool TryGetCertificateSubjectItem(this ITag topLevelTag, string oid, bool forIssuer, out List<string> items)
         {
             items = new List<string>();
-            if (IsOidNullOrEmpty(oid))
+            if (IsOidNullOrEmpty(oid) || !IsCertificate(topLevelTag))
                 return false;
-            /*
-             SEQUENCE               - tag
-                | SEQUENCE          - tag.Childs[0]
-                |  | [0]            - tag.Childs[0].Childs[0]
-                |  |  | INTEGER 2   - tag.Childs[0].Childs[0].Childs[0]
-                |  | INTEGER 36252270966868523741 tag.Childs[0].Childs[1]
-             */
-            /*
-             this.Childs[0]
-                    .Childs[0]: Version
-                    .Childs[1]: Serial Number
-                    .Childs[2]: Signature Algorithm
-                    .Childs[3]: Issuer
-                    .Childs[4]: Validity
-                        .Childs[0]:  - Not Before
-                        .Childs[1]:  - Not After
-                    .Childs[5]: Subject
-                    .Childs[6]: Public Key Information
-                        .Childs[0]:  - Public Key Algorithm
-                        .Childs[1]:  - Public Key
-                    .Childs[7]: Signature
-             */
-
-            try
-            {
-                if (topLevelTag.Childs.Count == 0)
-                    return false;   //("Tag cannot have zero childs", nameof(tag));
-                if (topLevelTag.Childs[0].Childs.Count == 0)
-                    return false;   //("First child tag cannot have zero childs", nameof(tag));
-                if (topLevelTag.Childs[0].Childs[0].Childs[0].TagNumber != (int)Tags.Tags.INTEGER)
-                    return false;   // ($"Certificate version tag was not found at position '{nameof(tag)}.Childs[0].Childs[0].Childs[0]'", nameof(tag));
-                if (HasValidSn(topLevelTag))
-                    return false;   //($"Certificate's serial number tag was not found at position '{nameof(tag)}.Childs[0].Childs[1]'", nameof(tag));
-                if (topLevelTag.Childs[0].Childs[4].Childs[0].TagNumber != (int)Tags.Tags.UTCTime)
-                    return false;   //($"Certificate's not after tag was not found at position '{nameof(tag)}.Childs[0].Childs[4].Childs[0]'", nameof(tag));
-            }
-            catch
-            {
-                return false;
-            }
 
             if (forIssuer)
             {
@@ -207,6 +181,83 @@ namespace Asn1DecoderNet5
 
         /// <summary>
         /// Attempts to find the value for requested OID. Certificate can contain more than one value for requested OID, the values are ordered in the 'items' list as they were found in the structure.
+        /// </summary>
+        /// <remarks>
+        /// The Subject element is parsed based on this structure:
+        /// <code>
+        /// this.Childs[0]
+        ///         .Childs[0]: Version
+        ///         .Childs[1]: Subject
+        ///         .Childs[2]: Subject Public Key Information
+        ///         .Childs[3]: Attributes
+        /// </code>
+        /// <br/>
+        /// <br/>
+        /// The OID is matched based on the PKCS10 CSR v1.7 specs, meaning the OID is expected to have a parent element of type SEQUENCE, which has a parent element of type SET, 
+        /// the value is taken from the second element of the SEQUENCE element.<br/>
+        /// Example of a correct SET element:
+        /// <code>
+        /// SET
+        ///  | SEQUENCE
+        ///  |  | OBJECT_IDENTIFIER 2.5.4.3, commonName, X.520 DN component
+        ///  |  | UTF8String common name value
+        /// </code>
+        /// </remarks>
+        /// <param name="topLevelTag">Tag object returned by <see cref="Decoder.Decode(byte[])"/></param>
+        /// <param name="subjectItem">Requested item</param>
+        /// <param name="items">The value</param>
+        /// <returns><see langword="true" /> if any value was found, otherwise <see langword="false" /></returns>
+        public static bool TryGetCertificateRequestSubjectItem(this ITag topLevelTag, SubjectItemKind subjectItem, out List<string> items)
+        {
+            return TryGetCertificateRequestSubjectItem(topLevelTag, subjectItem.ToOidString(), out items);
+        }
+
+        /// <summary>
+        /// Attempts to find the value for requested OID. Certificate can contain more than one value for requested OID, the values are ordered in the 'items' list as they were found in the structure.
+        /// </summary>
+        /// <remarks>
+        /// The Subject element is parsed based on this structure:
+        /// <code>
+        /// this.Childs[0]
+        ///         .Childs[0]: Version
+        ///         .Childs[1]: Subject
+        ///         .Childs[2]: Subject Public Key Information
+        ///         .Childs[3]: Attributes
+        /// </code>
+        /// <br/>
+        /// <br/>
+        /// The OID is matched based on the PKCS10 CSR v1.7 specs, meaning the OID is expected to have a parent element of type SEQUENCE, which has a parent element of type SET, 
+        /// the value is taken from the second element of the SEQUENCE element.<br/>
+        /// Example of a correct SET element:
+        /// <code>
+        /// SET
+        ///  | SEQUENCE
+        ///  |  | OBJECT_IDENTIFIER 2.5.4.3, commonName, X.520 DN component
+        ///  |  | UTF8String common name value
+        /// </code>
+        /// </remarks>
+        /// <param name="topLevelTag">Tag object returned by <see cref="Decoder.Decode(byte[])"/></param>
+        /// <param name="oid">Requested OID</param>
+        /// <param name="items">The value</param>
+        /// <returns><see langword="true" /> if any value was found, otherwise <see langword="false" /></returns>
+        public static bool TryGetCertificateRequestSubjectItem(this ITag topLevelTag, string oid, out List<string> items)
+        {
+            items = new List<string>();
+            try
+            {
+                if (IsOidNullOrEmpty(oid) || !IsCertificateRequest(topLevelTag))
+                    return false;
+                topLevelTag = topLevelTag.Childs[0].Childs[1];
+                return TryGetSubjectItem(topLevelTag, Encoding.OidEncoding.GetBytes(oid), out items);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to find the value for requested OID. Certificate/Request can contain more than one value for requested OID, the values are ordered in the 'items' list as they were found in the structure.
         /// </summary>
         /// <remarks>
         /// The OID is matched based on the X.509 v3, meaning the OID is expected to has a parent element of type SEQUENCE, which has a parent element of type SET, 
@@ -234,7 +285,7 @@ namespace Asn1DecoderNet5
         /// <remarks>
         /// The OID is matched based on the X.509 v3, meaning the OID is expected to has a parent element of type SEQUENCE, which has a parent element of type SET, 
         /// the value is taken from the second element of the SEQUENCE element.<br/>
-        /// Example of correct SET element:
+        /// Example of a correct SET element:
         /// <code>
         /// SET
         ///  | SEQUENCE
@@ -267,7 +318,7 @@ namespace Asn1DecoderNet5
                 }
                 foreach (var t in tag.Childs)
                 {
-                    bool isSet = IsSet(t);
+                    bool isSet = IsSET(t);
                     if (!isSet && t.Childs.Count > 0 && TryGetSubjectItem(t, oid, out var items2))
                     {
                         items.AddRange(items2);
@@ -285,7 +336,7 @@ namespace Asn1DecoderNet5
                 return false;
             }
             static bool IsValidSet(ITag t, byte[] oid)
-                => IsSet(t)
+                => IsSET(t)
                    && t.Childs.Count == 1
                    && t.Childs[0].TagNumber == (int)Tags.Tags.SEQUENCE
                    && t.Childs[0].Childs[0].Content.SequenceEqual(oid);
@@ -303,12 +354,13 @@ namespace Asn1DecoderNet5
         /// <param name="topLevelTag">Tag returned by <see cref="Decoder.Decode(byte[])"/> method</param>
         /// <param name="serialNumber">The serial number</param>
         /// <returns><see langword="true" /> if the element is found, otherwise <see langword="false" /></returns>
-        public static bool TryGetSerialNumber(this ITag topLevelTag, out string serialNumber)
+        public static bool TryCertificateGetSerialNumber(this ITag topLevelTag, out string serialNumber)
         {
             serialNumber = null;
             try
             {
-                if (!HasValidSn(topLevelTag))
+
+                if (!IsCertificate(topLevelTag))
                     return false;
                 serialNumber = GetValue(topLevelTag.Childs[0].Childs[1]);
                 return serialNumber is not null;
@@ -320,14 +372,64 @@ namespace Asn1DecoderNet5
         }
 
         #region Helpers
-        static bool IsSet(ITag t) => t.TagNumber == (int)Tags.Tags.SET;
+
+        /// <summary>
+        /// Checks wheter the ASN.1 structure in this tag corresponds to X.509 v3
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns><see langword="true"/> if the children structure corresponds to X.509 v3, otherwise <see langword="false"/></returns>
+        public static bool IsCertificate(this ITag tag)
+        {
+            try
+            {
+                return tag.Childs.Count == 3
+                           && tag.Childs[0].Childs.Count != 0
+                           && tag.Childs[0].Childs[0].TagClass == 2
+                           && tag.Childs[0].Childs[0].Childs[0].TagNumber == (int)Tags.Tags.INTEGER
+                           && tag.Childs[0].Childs[0].Childs[0].Content.SequenceEqual(_version2Sequence)
+                           && tag.Childs[1].TagNumber == (int)Tags.Tags.SEQUENCE
+                           && (tag.Childs[1].Childs.Count == 1 || tag.Childs[1].Childs.Count == 2)
+                           && tag.Childs[1].Childs[0].TagNumber == (int)Tags.Tags.OBJECT_IDENTIFIER
+                           && tag.Childs[2].Childs.Count == 0
+                           && tag.Childs[2].TagNumber == (int)Tags.Tags.BIT_STRING;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Checks wheter the ASN.1 structure in this tag corresponds to PKCS10 CSR v1.7 specification
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns><see langword="true"/> if the children structure corresponds to PKCS10 CSR v1.7 specification, otherwise <see langword="false"/></returns>
+        public static bool IsCertificateRequest(this ITag tag)
+        {
+            try
+            {
+                return tag.Childs.Count == 3
+                   && tag.Childs[0].Childs.Count != 0
+                   && tag.Childs[0].Childs[0].TagNumber == (int)Tags.Tags.INTEGER
+                   && tag.Childs[0].Childs[0].Content.SequenceEqual(_version0Sequence)
+                   && tag.Childs[1].TagNumber == (int)Tags.Tags.SEQUENCE
+                   && (tag.Childs[1].Childs.Count == 1 || tag.Childs[1].Childs.Count == 2)
+                   && tag.Childs[1].Childs[0].TagNumber == (int)Tags.Tags.OBJECT_IDENTIFIER
+                   && tag.Childs[2].Childs.Count == 0
+                   && tag.Childs[2].TagNumber == (int)Tags.Tags.BIT_STRING;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static bool IsSET(ITag t) => t.TagNumber == (int)Tags.Tags.SET;
         static string GetValue(ITag t)
         {
             if (t.Childs[0].Childs[1].ReadableContent is null)
                 t.Childs[0].Childs[1].ConvertContentToReadableContent();
             return t.Childs[0].Childs[1].ReadableContent;
         }
-        static bool HasValidSn(ITag t) => t.Childs[0].Childs[1].TagNumber != (int)Tags.Tags.INTEGER;
 
         static bool IsOidNullOrEmpty(string oid)
         {
